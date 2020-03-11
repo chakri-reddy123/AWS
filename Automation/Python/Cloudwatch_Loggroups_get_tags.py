c1403a3da21ec936d948af2e82ea6f05b7f7766f
wanace <40069749+wanace@users.noreply.github.com>
2020-03-11 15:09:21 -0500

    updated

--- Automation/Python/Cloudwatch_Loggroups_get_tags.py
+++ Automation/Python/Cloudwatch_Loggroups_get_tags.py
@@ -6,5 +6,5 @@ region = 'us-east-1'
     
 def lambda_handler(event, context):
   cwlogs = boto3.client('logs', region)
-  response = cwlogs.list_tags_log_group(logGroupName='test1')
+  response = cwlogs.list_tags_log_group(logGroupName='Add Cloudwatch Log Group Name')
   print response
